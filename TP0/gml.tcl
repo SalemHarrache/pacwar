e6cbbb821e4a9737a30a15461861a0a002488438
Salem Harrache <salem@harrache.info>
2012-10-29 15:28:35 +0100

    gmlObject fonctionnel

--- TP0/gml.tcl
+++ TP0/gml.tcl
@@ -2,5 +2,30 @@
 # restart using tclsh \
 exec tclsh "$0" "$@"
 
-# load utilities
-source gml_Object.tcl
+# load lib
+source [file join [file dirname [info script]] .. lib init.tcl]
+
+
+method Rectangle area {} { return [expr $this(width) * $this(height)] }
+
+method Rectangle constructor {width height} {
+   set this(width) $width
+   set this(height) $height
+}
+
+# Héritage
+inherit carre Rectangle
+
+
+method carre constructor {width} {
+    # Super
+   this inherited $width $width
+}
+
+carre carre_obj 12
+
+puts [carre_obj area]
+
+puts $carre_obj(width)
+
+carre_obj dispose
