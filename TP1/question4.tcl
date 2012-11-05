26c51e63f76713f4ca44b8af13c691d7f86bde15
Salem Harrache <salem@harrache.info>
2012-11-05 21:28:57 +0100

    TP1 : FormuleAbstraction

--- TP1/question4.tcl
+++ TP1/question4.tcl
@@ -9,13 +9,28 @@ source [file join [file dirname [info script]] . question2.tcl]
 source [file join [file dirname [info script]] . PVNRT.tcl]
 
 inherit FormuleAbstraction Abstraction
-method FormuleAbstraction constructor {control value} {
+method FormuleAbstraction constructor {control} {
    this inherited $control
-   set this(value) $value
-   trace add variable this(value) write "$objName change"
+   lassign [PVNRT::equi 1 1 1] this(pressure) this(volume) this(temperature)
+   trace add variable this(pressure) write "$objName change"
+   trace add variable this(volume) write "$objName change"
+   trace add variable this(temperature) write "$objName change"
 }
 
+method FormuleAbstraction change {varname} {
+   $this(control) change
+}
 
+method FormuleAbstraction edit {varname value} {
+	if {$varname == "temperature"} {
+		lassign [PVNRT::equi $this(pressure) $this(volume) value isotherme] this(pressure) this(volume) this(temperature)
+	} elseif {$varname == "isochore"} {
+		lassign [PVNRT::equi $this(pressure) value $this(temperature) isobare] this(pressure) this(volume) this(temperature)
+	} elseif {$varname == "pressure"} {
+		lassign [PVNRT::equi value $this(volume) $this(temperature) isobare] this(pressure) this(volume) this(temperature)
+	}
+   	$this(control) change
+}
 
 
 # main --
