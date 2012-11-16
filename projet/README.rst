49598cf5afd5fa06d9150fe04fbc1f788e477160
Salem Harrache <salem@harrache.info>
2012-11-16 09:27:34 +0100

    modification du script run.sh pour lancer introspac

---
+++ projet/README.rst
@@ -0,0 +1,15 @@
+PACWar
+======
+
+
+Pour lancer le jeu :
+
+:
+
+  ./run.sh
+
+pour lancer le jeu avec introspact: 
+
+:
+
+  ./run.sh introspac
---
+++ projet/run.sh
@@ -0,0 +1,5 @@
+if [ $# -ne 1 ]; then
+    wish ../lib/tkcon.tcl -load Tk app.tcl
+else
+    wish ../lib/tkcon.tcl -load Tk app_introspac.tcl
+fi
