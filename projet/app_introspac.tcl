783a61c2396ead9219f9012fd0612b1fffc57679
Salem Harrache <salem@harrache.info>
2012-11-23 12:15:06 +0100

    Test avec Introspact

--- projet/app_introspac.tcl
+++ projet/app_introspac.tcl
@@ -1,15 +1,4 @@
 
+source app.tcl
 
-# source app.tcl
-# Pour l'instant on affiche juste celui du tp 1 pour voir
-source [file join [file dirname [info script]] .. lib init.tcl]
-source [file join [file dirname [info script]] .. TP1 question6.tcl]
-
-# executed only if the file is the main script
-FormuleControl agent_central
-
-TemperatureControlCelsius agent_temp_c agent_central
-PressureControl agent_press agent_central
-VolumeControl agent_volume agent_central
-
-Introspact introspact agent_central
+Introspact introspact game
