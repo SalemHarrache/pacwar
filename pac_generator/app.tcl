f65a0fcf5a45328261e2c867bc35da3d3a39929c
Salem Harrache <salem@harrache.info>
2012-11-29 22:14:17 +0100

    Test du generateur avec AgentValue

--- pac_generator/app.tcl
+++ pac_generator/app.tcl
@@ -4,39 +4,21 @@ exec wish "$0" "$@"
 
 # load utilities
 source [file join [file dirname [info script]] .. lib init.tcl]
-source pac_generator.tcl
 
-
-inherit ValueControl Control
-
-
-inherit ValueAbstraction Abstraction
-inherit ValuePresentation Presentation
-
-
-Generate_PAC_accessors ValueControl ValueAbstraction ValuePresentation value
-
-
-# ValueAbstraction --
-method ValueAbstraction constructor {control} {
-   this inherited $control
-}
-
-method ValueAbstraction destructor {} {
-   this inherited
-}
+generate_pac_agent Value
+generate_pac_accessors Value value
 
 
 # ValuePresentation --
-method ValuePresentation constructor {control label_name unit_name} {
+method ValuePresentation constructor {control} {
    this inherited $control
 
    set this(window) .
-   set this(label) [label $this(window).label -text $label_name -justify center]
+   set this(label) [label $this(window).label -text "Température" -justify center]
 
    set this(unit_layout) [frame $this(window).frame]
    set this(entry) [entry $this(window).entry -justify center]
-   set this(unit) [label $this(window).unit -text $unit_name -justify center]
+   set this(unit) [label $this(window).unit -text "°C" -justify center]
 
    pack $this(label) -expand 1 -fill both
    pack $this(entry) $this(unit) -side left -padx 4
@@ -49,20 +31,12 @@ method ValuePresentation set_value {value} {
    $this(entry) insert 0 $value
 }
 
-method ValuePresentation destructor {} {
-   this inherited
-}
-
 # ValueControl --
-method ValueControl constructor {parent value label unit} {
-   ValuePresentation ${objName}_pres $objName $label $unit
+method ValueControl constructor {parent value} {
+   ValuePresentation ${objName}_pres $objName
    ValueAbstraction ${objName}_abst $objName
    this inherited $parent ${objName}_abst ${objName}_pres
    $this(abstraction) set_value $value
 }
 
-method ValueControl destructor {} {
-   this inherited
-}
-
-ValueControl control_value "" 10 Température "°C"
+ValueControl control_value "" 10
