8165cd5a664cf9ac1ae056badb29c0200c29b7a4
Salem Harrache <salem@harrache.info>
2012-12-17 20:36:06 +0100

    Personalisation de la canvas_mini_map directement par l'argent MiniMapUniverse
    Pareil pour l'agent MapUniverse

--- game.tcl
+++ game.tcl
@@ -1,6 +1,4 @@
 source [file join [file dirname [info script]] . lib init.tcl]
-set ressources_dir [file join [file dirname [info script]] ressources \
-                    universe]
 
 generate_pac_agent "Game"
 
@@ -16,25 +14,18 @@ method GamePresentation init {} {
     wm maxsize $this(tk_parent) 900 600
     wm title $this(tk_parent) "PacWar !"
 
-    this set_canvas_map [canvas $this(tk_parent).canvas_map -width 400 \
-                            -height 200 -background "#191919" ]
+    this set_canvas_map [canvas $this(tk_parent).canvas_map]
     this set_frame_panel  [frame $this(tk_parent).frame_panel -relief raised \
                             -borderwidth 1]
     # On ajoute les deux map dans une frame
-    this set_canvas_mini_map [canvas $this(frame_panel).canvas_mini_map \
-                                -width 200 -height 200 -background "#1E1E1E"]
+    this set_canvas_mini_map [canvas $this(frame_panel).canvas_mini_map]
     this set_canvas_panel [canvas $this(frame_panel).canvas_panel -width 200 \
                             -height 200 -background "#383838"]
 
 
-    set background [image create photo -file ./ressources/universe/background.png]
-    $this(canvas_map) create image 0 0 -anchor nw -image $background
+
     pack $this(canvas_map) -expand 1 -side right -fill both
-
     pack $this(frame_panel) -side left -fill both
-
-    set background_mini [image create photo -file ./ressources/universe/background_mini.jpg]
-    $this(canvas_mini_map) create image 0 0 -anchor nw -image $background_mini
     pack $this(canvas_panel) -expand 1 -fill both
     pack $this(canvas_mini_map) -fill both
 
--- universe.tcl
+++ universe.tcl
@@ -1,19 +1,37 @@
-source [file join [file dirname [info script]] . lib init.tcl]
 
-
+# Génération d'un agent pac multivue : MapUniverse et MiniMapUniverse
 generate_pac_agent_multi_view "Universe" [list "Map" "MiniMap"]
 
+# Génération d'un methode pour acceder au canvas_map parent depuis universe.
 generate_pac_parent_accessors Universe canvas_map
-generate_pac_accessors MapUniverse canvas_map
+generate_pac_parent_accessors Universe canvas_mini_map
+# Génération d'un methode pour acceder au canvas_map parent depuis les deux
+# vues de l'argent universe : MapUniverse et MiniMapUniverse
 generate_pac_parent_accessors MapUniverse canvas_map
+generate_pac_parent_accessors MiniMapUniverse canvas_mini_map
+# Generation des accesseurs pour les attributs
+generate_pac_presentation_accessors MapUniverse canvas_map
+generate_pac_presentation_accessors MiniMapUniverse canvas_mini_map
+
 
 
 method MapUniversePresentation init {} {
-    # this set_canvas_map [$this(control) get_parent_canvas_map]
-    # this set_canvas_map [$this(parent) get_canvas_map]
+    global ressources_dir
+    this set_canvas_map [$this(control) get_parent_canvas_map]
+    $this(canvas_map) configure -width 400 -height 200 -background "#191919"
 
-    # $this(canvas_map)
+    set background_file [file join $ressources_dir universe background.png]
+    set background [image create photo -file $background_file]
+    $this(canvas_map) create image 0 0 -anchor nw -image $background
+}
 
-    # [canvas $this(tk_parent).canvas_map -width 400 \
-    #                         -height 200 -background "#191919" ]
+
+method MiniMapUniversePresentation init {} {
+    global ressources_dir
+    this set_canvas_mini_map [$this(control) get_parent_canvas_mini_map]
+    $this(canvas_mini_map) configure -width 200 -height 200 -background "#1E1E1E"
+
+    set background_file [file join $ressources_dir universe background_mini.jpg]
+    set background [image create photo -file $background_file]
+    $this(canvas_mini_map) create image 0 0 -anchor nw -image $background
 }
