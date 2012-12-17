195bdc4a8c31ca5584143b7c7d2b67a1bdbe3f57
Salem Harrache <salem@harrache.info>
2012-12-17 20:56:52 +0100

    Deplacer le code de personalisation de la vue panel dans l'agent PanelPresentaiton (au lieux de game)

--- game.tcl
+++ game.tcl
@@ -1,11 +1,13 @@
-source [file join [file dirname [info script]] . lib init.tcl]
-
 generate_pac_agent "Game"
 
+# Sert a afficher la map
 generate_pac_presentation_accessors Game canvas_map
-generate_pac_presentation_accessors Game frame_panel
+# Sert a afficher la mini map
 generate_pac_presentation_accessors Game canvas_mini_map
-generate_pac_presentation_accessors Game canvas_panel
+# Sert a afficher le panel
+generate_pac_presentation_accessors Game frame_panel
+# Sert juste à regrouper le panel et la mini map
+generate_pac_presentation_accessors Game frame_wrapper
 
 
 method GamePresentation init {} {
@@ -15,18 +17,14 @@ method GamePresentation init {} {
     wm title $this(tk_parent) "PacWar !"
 
     this set_canvas_map [canvas $this(tk_parent).canvas_map]
-    this set_frame_panel  [frame $this(tk_parent).frame_panel -relief raised \
-                            -borderwidth 1]
-    # On ajoute les deux map dans une frame
-    this set_canvas_mini_map [canvas $this(frame_panel).canvas_mini_map]
-    this set_canvas_panel [canvas $this(frame_panel).canvas_panel -width 200 \
-                            -height 200 -background "#383838"]
-
-
+    this set_frame_wrapper [frame $this(tk_parent).frame_wrapper -relief raised -borderwidth 1]
+    # On regroupe les le panel et la minimap dans une frame
+    this set_canvas_mini_map [canvas $this(frame_wrapper).canvas_mini_map]
+    this set_frame_panel [frame $this(frame_wrapper).frame_panel]
 
     pack $this(canvas_map) -expand 1 -side right -fill both
-    pack $this(frame_panel) -side left -fill both
-    pack $this(canvas_panel) -expand 1 -fill both
+    pack $this(frame_wrapper) -side left -fill both
+    pack $this(frame_panel) -expand 1 -fill both
     pack $this(canvas_mini_map) -fill both
 
 }
--- panel.tcl
+++ panel.tcl
@@ -1,10 +1,12 @@
-source [file join [file dirname [info script]] . lib init.tcl]
-
-
-
+# Génération d'un agent Panel
 generate_pac_agent "Panel"
 
+generate_pac_parent_accessors "Panel" frame_panel
+generate_pac_presentation_accessors "Panel" frame_panel
 
 method PanelPresentation init {} {
-
+    this set_frame_panel [$this(control) get_parent_frame_panel]
+    set this(label) [label $this(frame_panel).label -justify center -text "Joueur X"]
+    pack configure $this(label) -expand 1 -fill both
+    # $this(frame_map) configure -width 400 -height 200 -background "#191919"
 }
