e683ee9c5c3de5dfe38eacc5a883110ea227a2d9
Salem Harrache <salem@harrache.info>
2012-12-21 09:00:38 +0100

    Ajout d'un mode d'affichage. En effet on comme pour température on peut changer la disposition du panel avec CTRL+P (utile pour demontrer pac)

--- game.tcl
+++ game.tcl
@@ -8,13 +8,19 @@ generate_pac_presentation_accessors Game canvas_mini_map
 generate_pac_presentation_accessors Game frame_panel
 # Sert juste à regrouper le panel et la mini map
 generate_pac_presentation_accessors Game frame_wrapper
+# Sert a afficher la mini map
+generate_pac_presentation_accessors Game display_mode
+
+
+
 
 
 method GamePresentation init {} {
     wm aspect $this(tk_parent) 3 2 3 2
-    wm minsize $this(tk_parent) 900 600
-    wm maxsize $this(tk_parent) 900 600
+    # wm minsize $this(tk_parent) 1200 1200
+    # wm maxsize $this(tk_parent) 1200 1200
     wm title $this(tk_parent) "PacWar !"
+    this set_display_mode vertical
 
     this set_canvas_map [canvas $this(tk_parent).canvas_map]
     this set_frame_wrapper [frame $this(tk_parent).frame_wrapper -relief raised -borderwidth 1]
@@ -27,9 +33,38 @@ method GamePresentation init {} {
     pack $this(frame_panel) -expand 1 -fill both
     pack $this(canvas_mini_map) -fill both
 
+    bind $this(tk_parent) <Control-Key-p> "$objName switch_view_mode; $objName refresh"
+    wm minsize $this(tk_parent) 0 0
+    wm maxsize $this(tk_parent) 0 0
+}
+
+method GamePresentation switch_view_mode {} {
+    if {[this get_display_mode] == "vertical"} {
+        this set_display_mode "horizontal"
+    } else {
+        this set_display_mode "vertical"
+    }
+}
+
+method GamePresentation refresh {} {
+    pack forget $this(canvas_map) $this(frame_wrapper) $this(frame_panel) $this(canvas_mini_map)
+    if {$this(display_mode) == "vertical"} {
+        pack configure $this(canvas_map)  -expand 1 -side right -fill both
+        pack configure $this(frame_wrapper) -side left -fill both
+        pack configure $this(frame_panel) -expand 1 -fill both
+        pack configure $this(canvas_mini_map) -fill both
+    } else {
+        pack configure $this(frame_wrapper) -side right -fill both
+        pack configure $this(frame_panel) -expand 1 -fill both
+        pack configure $this(canvas_mini_map) -fill both
+        pack configure $this(canvas_map)  -expand 1 -side right -fill both
+    }
+
+
 }
 
 
 method GameControl add_player {player} {
 
 }
+
