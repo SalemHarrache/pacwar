951cf862fb99b5d228ba4557557bb4b7df43b0c5
Salem Harrache <salem@harrache.info>
2012-12-27 23:01:44 +0100

    mute et unmute marchent

--- sound.tcl
+++ sound.tcl
@@ -6,28 +6,49 @@ generate_pac_accessors "Sound" battle_sound
 generate_pac_accessors "Sound" end_sound
 
 # ==> left volume
+generate_pac_accessors "Sound" mode
 generate_pac_accessors "Sound" volume
-generate_pac_accessors "Sound" rightvol
+
+
 
 
 method SoundAbstraction init {} {
     this set_main_sound [snack::sound main_sound -file [abspath ressources sound main.mp3]]
     this set_battle_sound [snack::sound battle_sound -file [abspath ressources sound battle.mp3]]
     this set_end_sound [snack::sound end_sound -file [abspath ressources sound end.mp3]]
-
+    this set_mode "main"
     this unmute
-    loop_sound $this(main_sound)
+}
 
-    snack::mixer volume Vol $this(volume) $this(rightvol)
+method SoundAbstraction stop_all {} {
+    $this(main_sound) stop
+    $this(battle_sound) stop
+    $this(end_sound) stop
+}
+
+method SoundAbstraction switch_mode {mod} {
+    this stop_all
+    this set_mode $mod
+    if {$mod=="main"} {
+        loop_sound $this(main_sound)
+    } else {
+        if {$mod=="battle"} {
+            loop_sound $this(battle_sound)
+        } else {
+            if {$mod=="end"} {
+                $this(end_sound) play
+            }
+        }
+    }
 }
 
 method SoundAbstraction mute {} {
-    this set_rightvol 0
+    this stop_all
     this set_volume 0
 }
 
 method SoundAbstraction unmute {} {
-    this set_rightvol 100
+    this switch_mode $this(mode)
     this set_volume 100
 }
 
@@ -43,11 +64,3 @@ method SoundControl toggle_sound {} {
 method SoundControl system_change_volume { v } {
     $this(parent) sound_changed $v
 }
-
-# method SoundControl system_change_volume {} {
-#     puts "a implementer"
-# }
-
-# method SoundControl toggle_sound {} {
-
-# }
