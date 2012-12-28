506a288595be0d1f37dac90f53a23616727195aa
Salem Harrache <salem@harrache.info>
2012-12-28 16:16:01 +0100

    snipet tclracer

--- snippet_tk/canvas_to_png.tcl
+++ snippet_tk/canvas_to_png.tcl
@@ -1,124 +1,286 @@
- proc CanvasToPic { canvasID } {
-           set returnThis [catch {package require Img} version]
-       #Gets the contents of the canvas canvasID and put into photo image photoimage.
-       set returnThis [catch {image create photo -format window -data $canvasID} photoimage]
-       if { $returnThis != 0 } {
-          puts "\nERROR: Cannot create photo!!!"
-          exit 1
-       }
-       return $photoimage
+#### setup
+package require Img
+package require Tk
+package require XOTcl
+namespace import xotcl::*
+
+
+#### globals
+set ::pi  [expr 4 * atan(1)]
+set ::deg [expr $pi / 180]
+
+# z coordinate of the vector product of a triangle
+proc zVecProd {p1x p1y p2x p2y p3x p3y} {
+   return [expr ($p1x - $p3x) * ($p2y - $p3y) - \
+                ($p1y - $p3y) * ($p2x - $p3x)]
+}
+
+# return true if the triangles PQR1 and PQR2 have edge intersection
+proc edgeIntersect {p1x p1y q1x q1y r1x r1y p2x p2y q2x q2y r2x r2y} {
+
+  if {[zVecProd $r2x $r2y $p2x $p2y $q1x $q1y] >= 0.0} {
+
+    if {[zVecProd $p1x $p1y $p2x $p2y $q1x $q1y] >= 0.0} {
+        return [expr [zVecProd $p1x $p1y $q1x $q1y $r2x $r2y] >= 0.0]
+    }
+
+    return [expr {[zVecProd $q1x $q1y $r1x $r1y $p2x $p2y] >= 0.0 &&
+                  [zVecProd $r1x $r1y $p1x $p1y $p2x $p2y] >= 0.0}]
+  }
+
+  if {[zVecProd $r2x $r2y $p2x $p2y $r1x $r1y] < 0.0} {
+     return 0
+  }
+
+  if {[zVecProd $p1x $p1y $p2x $p2y $r1x $r1y] >= 0.0} {
+
+     return [expr {[zVecProd $p1x $p1y $r1x $r1y $r2x $r2y] >= 0.0 ||
+                   [zVecProd $q1x $q1y $r1x $r1y $r2x $r2y] >= 0.0}]
+  }
+
+  return 0
+}
+
+# return true if triangle 1 (pqr) intersects 2 (pqr) via a vertex
+proc vertIntersect {p1x p1y q1x q1y r1x r1y p2x p2y q2x q2y r2x r2y} {
+
+   if {[zVecProd $r2x $r2y $p2x $p2y $q1x $q1y] >= 0.0} {
+
+      if {[zVecProd $r2x $r2y $q2x $q2y $q1x $q1y] <= 0.0} {
+
+         if {[zVecProd $p1x $p1y $p2x $p2y $q1x $q1y] > 0.0} {
+
+            return [expr {[zVecProd $p1x $p1y $q2x $q2y $q1x $q1y] <= 0.0}]
+         }
+
+         return [expr {[zVecProd $p1x $p1y $p2x $p2y $r1x $r1y] >= 0.0 &&
+                       [zVecProd $q1x $q1y $r1x $r1y $p2x $p2y] >= 0.0}]
+      }
+
+      return [expr {[zVecProd $p1x $p1y $q2x $q2y $q1x $q1y] <= 0.0 &&
+                    [zVecProd $r2x $r2y $q2x $q2y $r1x $r1y] <= 0.0 &&
+                    [zVecProd $q1x $q1y $r1x $r1y $q2x $q2y] >= 0.0}]
    }
 
-   canvas .c1 -width 460 -height 350 -bg white -relief groove \
-           -borderwidth 4
-   grid .c1 -row 1 -column 1 -columnspan 2
-   frame .f1
-   button .f1.b1 -text "Selection Board" -command {
-   .c1 create line 55 0 55 350 -width 2
-   for { set q 1 } { $q <= 21 } { incr q } {
-   .c1 create rect 10 10 30 30 -fill red -tag movable
-   .c1 create poly 30 10 30 30 50 30 -fill blue -tag movable -outline black
-   .c1 create poly 30 10 50 10 50 30 -fill blue -tag movable -outline black
-
-   .c1 create rect 10 30 30 50 -fill red -tag movable
-   .c1 create poly 30 30 30 50 50 30 -fill blue -tag movable -outline black
-   .c1 create poly 50 30 30 50 50 50 -fill blue -tag movable -outline black
-
-   .c1 create rect 10 50 30 70 -fill blue -tag movable
-   .c1 create poly 30 50 30 70 50 50 -fill red -tag movable -outline black
-   .c1 create poly 50 50 30 70 50 70 -fill red -tag movable -outline black
-
-   .c1 create rect 10 70 30 90 -fill blue -tag movable
-   .c1 create poly 30 70 30 90 50 90 -fill red -tag movable -outline black
-   .c1 create poly 30 70 50 70 50 90 -fill red -tag movable -outline black
-
-
-   .c1 create rect 10 90 30 110 -fill green -tag movable
-   .c1 create poly 30 90 30 110 50 110 -fill yellow -tag movable -outline black
-   .c1 create poly 30 90 50 90 50 110 -fill yellow -tag movable -outline black
-   .c1 create poly 30 50 30 70 50 50 -fill red -tag movable -outline black
-   .c1 create poly 50 50 30 70 50 70 -fill red -tag movable -outline black
-
-   .c1 create rect 10 110 30 130 -fill green -tag movable
-   .c1 create poly 30 110 30 130 50 130 -fill yellow -tag movable -outline black
-   .c1 create poly 30 110 50 110 50 130 -fill yellow -tag movable -outline black
-
-   .c1 create rect 10 130 30 150 -fill yellow -tag movable
-   .c1 create poly 30 130 30 150 50 130 -fill green -tag movable -outline black
-   .c1 create poly 50 130 30 150 50 150 -fill green -tag movable -outline black
-
-   .c1 create rect 10 150 30 170 -fill yellow -tag movable
-   .c1 create poly 30 150 30 170 50 150 -fill green -tag movable -outline black
-   .c1 create poly 50 150 30 170 50 170 -fill green -tag movable -outline black
-
-   .c1 create rect 10 170 30 190 -fill lightgreen -tag movable
-   .c1 create rect 30 170 50 190 -fill lightblue -tag movable
-
-   .c1 create rect 10 190 30 210 -fill black -tag movable
-   .c1 create rect 30 190 50 210 -fill white -tag movable -outline black
-
-   .c1 create rect 10 210 30 230 -fill brown -tag movable
-   .c1 create rect 30 210 50 230 -fill orange -tag movable -outline black
-
-   .c1 create rect 10 230 30 250 -fill black -tag movable
-   .c1 create rect 30 230 50 250 -fill white -tag movable -outline black
-
-   .c1 create rect 10 250 30 270 -fill brown -tag movable
-   .c1 create rect 30 250 50 270 -fill orange -tag movable -outline black
-
-   .c1 create rect 10 270 30 290 -fill cyan -tag movable
-   .c1 create rect 30 270 50 290 -fill grey -tag movable -outline black
-
-   .c1 create rect 10 290 30 310 -fill magenta -tag movable
-   .c1 create rect 30 290 50 310 -fill purple -tag movable -outline black
-
-
-   .c1 create oval 10 310 30 330 -fill brown -tag movable
-   .c1 create oval 30 310 50 330 -fill orange -tag movable -outline black
-
-   .c1 create oval 10 330 30 350 -fill red -tag movable
-   .c1 create oval 30 330 50 350 -fill blue -tag movable -outline black
-   }
-   .f1.b1 configure -state disabled
+   if {[zVecProd $r2x $r2y $p2x $p2y $r1x $r1y] < 0.0} {
+      return 0
    }
 
-   button .f1.b2 -text "Clear All" -command {
-   .c1 delete all
-   .f1.b1 configure -state normal
+   if {[zVecProd $q1x $q1y $r1x $r1y $r2x $r2y] >= 0.0} {
+
+      return [expr {[zVecProd $p1x $p1y $p2x $p2y $r1x $r1y] >= 0.0}]
    }
 
-   button .f1.b3 -text "MakePngImage" -command {
-   set photoimage [CanvasToPic .c1]
-   puts $photoimage
-   puts "Writing photoimage in PNG format: photoimage.png"
-   $photoimage write photoimage.png -format PNG
+   return [expr {[zVecProd $q1x $q1y $r1x $r1y $q2x $q2y] >= 0.0 &&
+                 [zVecProd $r2x $r2y $r1x $r1y $q2x $q2y] >= 0.0}]
+}
+
+# return true if triangle 1 (pqr) intersects triange 2 (pqr)
+proc triIntersect {p1x p1y q1x q1y r1x r1y p2x p2y q2x q2y r2x r2y} {
+
+   if {[zVecProd $p2x $p2y $q2x $q2y $p1x $p1y] >= 0.0} {
+
+      if {[zVecProd $q2x $q2y $r2x $r2y $p1x $p1y] >= 0.0} {
+
+         if {[zVecProd $r2x $r2y $p2x $p2y $p1x $p1y] >= 0.0} {
+            return 1
+         }
+
+         return [edgeIntersect $p1x $p1y $q1x $q1y $r1x $r1y $p2x $p2y $q2x $q2y $r2x $r2y]
+      }
+
+      if {[zVecProd $r2x $r2y $p2x $p2y $p1x $p1y] >= 0.0} {
+         return [edgeIntersect $p1x $p1y $q1x $q1y $r1x $r1y $r2x $r2y $p2x $p2y $q2x $q2y]
+      }
+
+      return [vertIntersect $p1x $p1y $q1x $q1y $r1x $r1y $p2x $p2y $q2x $q2y $r2x $r2y]
    }
 
-   pack .f1.b1 -side left
-   pack .f1.b2 -side left
-   pack .f1.b3 -side left
-   grid .f1 -row 2 -column 1
+   if {[zVecProd $q2x $q2y $r2x $r2y $p1x $p1y] >= 0.0} {
+      if {[zVecProd $r2x $r2y $p2x $p2y $p1x $p1y] >= 0.0} {
+         return [edgeIntersect $p1x $p1y $q1x $q1y $r1x $r1y $q2x $q2y $r2x $r2y $p2x $p2y]
+      }
 
-   proc CanvasMarkIt { x y can } {
-   global canvas
-   $can raise current
-   set x [$can canvasx $x]
-   set y [$can canvasy $y]
-   set canvas($can,obj) [ $can find closest $x $y ]
-   set canvas($can,x) $x
-   set canvas($can,y) $y
+      return [vertIntersect $p1x $p1y $q1x $q1y $r1x $r1y $q2x $q2y $r2x $r2y $p2x $p2y]
    }
 
-   proc CanvasDragIt { x y can } {
-   global canvas
-   set x [$can canvasx $x]
-   set y [$can canvasy $y]
-   set dx [expr $x - $canvas($can,x)]
-   set dy [expr $y - $canvas($can,y)]
-   $can move $canvas($can,obj) $dx $dy
-   set canvas($can,x) $x
-   set canvas($can,y) $y
+   return [vertIntersect $p1x $p1y $q1x $q1y $r1x $r1y $r2x $r2y $p2x $p2y $q2x $q2y]
+}
+
+#### Car class
+Class create ^Car
+
+^Car instproc init {{x 15} {y 10}} {
+   my instvar carAng carSpeed carX carY
+
+   set carAng   [expr 5 * $::pi / 8]
+   set carSpeed  0
+   set carX     $x
+   set carY     $y
+}
+
+^Car instproc checkWalls {ret} {
+   my instvar carAng carSpeed carX carY
+
+   set minX [expr min([lindex $ret 0],[lindex $ret 2],[lindex $ret 4])]
+   set minY [expr min([lindex $ret 1],[lindex $ret 3],[lindex $ret 5])]
+
+   set maxX [expr max([lindex $ret 0],[lindex $ret 2],[lindex $ret 4])]
+   set maxY [expr max([lindex $ret 1],[lindex $ret 3],[lindex $ret 5])]
+
+   set cwidth  [.c cget -width]
+   set cheight [.c cget -height]
+
+   if {$minX < 0} {
+      set carX [expr $carX - $minX]
+      set carSpeed 0
+   } elseif {$maxX > $cwidth} {
+      set carX [expr $carX - $maxX + $cwidth]
+      set carSpeed 0
    }
 
-   .c1 bind movable <Button-1>  {CanvasMarkIt %x %y %W}
-   .c1 bind movable <B1-Motion> {CanvasDragIt %x %y %W}
+   if {$minY < 0} {
+      set carY [expr $carY - $minY]
+      set carSpeed 0
+   } elseif {$maxY > $cheight} {
+      set carY [expr $carY - $maxY + $cheight]
+      set carSpeed 0
+   }
+}
+
+# construct a triplet of coords
+^Car instproc makeCar {} {
+   my instvar carAng carSpeed carX carY
+
+   set r 10 ;# car "radius" (size)
+   set t $carAng
+   set offX $carX
+   set offY $carY
+
+   set ret ""
+
+   # point 1, right side
+   lappend ret [expr $r * cos($t+90*$::deg) + $offX]
+   lappend ret [expr $r * sin($t+90*$::deg) + $offY]
+
+   # point 2, left side
+   lappend ret [expr $r * cos($t-90*$::deg) + $offX]
+   lappend ret [expr $r * sin($t-90*$::deg) + $offY]
+
+   # point 3, front
+   lappend ret [expr 2*$r * cos($t) + $offX]
+   lappend ret [expr 2*$r * sin($t) + $offY]
+
+   return $ret
+}
+
+^Car instproc move {} {
+   my instvar carX carY carSpeed carAng
+
+   # adjust position for speed
+   set carX [expr $carX + $carSpeed/4.0 * cos($carAng)]
+   set carY [expr $carY + $carSpeed/4.0 * sin($carAng)]
+}
+
+^Car instproc accel {{amt 1}} {
+   my instvar carSpeed
+   incr carSpeed $amt
+}
+
+^Car instproc incrAng {} {
+   my instvar carAng carSpeed
+
+   set amt [expr (abs($carSpeed)+3) / 4.0 * $::deg]
+   set carAng [expr $carAng + $amt]
+   if {$carAng >= 2 * $::pi} {
+      set carAng 0
+   }
+}
+
+^Car instproc decrAng {} {
+   my instvar carAng carSpeed
+
+   set amt [expr (abs($carSpeed)+3) / 4.0 * $::deg]
+   set carAng [expr $carAng - $amt]
+   if {$carAng < 0} {
+      set carAng [expr 2 * $::pi]
+   }
+}
+
+#### cars
+^Car create p1car ;# player 1 car
+^Car create ccar 200 200;# computer car
+
+
+#### event loop
+proc eventLoop {} {
+   p1car move
+   ccar move
+
+   set c1loc [p1car makeCar]
+   set c2loc [ccar makeCar]
+   p1car checkWalls $c1loc
+   ccar  checkWalls $c2loc
+
+   if {[triIntersect {*}$c1loc {*}$c2loc]} {
+      p1car set carSpeed 0
+      ccar  set carSpeed 0
+   }
+
+   # redraw car
+   .c coords $::carId  $c1loc
+   .c coords $::ccarId $c2loc
+
+   after 30 eventLoop
+}
+
+proc abspath {arg args} {
+    set cmd "return \[file normalize \[file join $arg $args]]"
+    eval $cmd
+}
+
+proc get_new_universe_bg {{num 0}} {
+  set path [abspath .. ressources universe "universe[expr $num % 3].jpg"]
+  return [image create photo -file $path]
+}
+
+pack [canvas .c] -side top -expand 1 -fill both
+
+proc initBackground {canvas sourceImage} {
+    set tiledImage [image create photo]
+    $canvas create image 0 0  -anchor nw  -image $tiledImage  -tags {backgroundBitmap}
+    $canvas lower backgroundBitmap
+    if {0} {
+        proc {tile} {canvas sourceImage tiledImage} {
+            $tiledImage copy $sourceImage  -to 0 0 [winfo width $canvas] [winfo height $canvas]
+        }
+        bind $canvas <Configure> [list tile $canvas $sourceImage $tiledImage]
+        bind $canvas <Destroy> [list image delete $sourceImage $tiledImage]
+        tile $canvas $sourceImage $tiledImage
+    } else {
+        # avoid needing a tile proc
+        bind $canvas <Configure> "$tiledImage copy $sourceImage  -to 0 0 \[winfo width $canvas\] \[winfo height $canvas\]"
+        bind $canvas <Destroy> [list image delete $sourceImage $tiledImage]
+        $tiledImage copy $sourceImage  -to 0 0 [winfo width $canvas] [winfo height $canvas]
+    }
+}
+
+initBackground .c [get_new_universe_bg]
+
+set ::carId  [.c create poly [p1car makeCar] -fill blue -tags car]
+set ::ccarId [.c create poly [ccar makeCar] -fill red -tags car]
+
+bind .c <Left>  {p1car decrAng}
+bind .c <Right> {p1car incrAng}
+bind .c <Up>    {p1car accel}
+bind .c <Down>  {p1car accel -1}
+bind .c <Enter> {focus %W}
+
+bind .c <Configure> {
+   %W configure -width  [winfo width  .c]
+   %W configure -height [winfo height .c]
+}
+
+initBackground .c [get_new_universe_bg]
+
+eventLoop
