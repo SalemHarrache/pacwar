e43002e9f80fcfa7a9be1ba6554a906fe483a652
Salem Harrache <salem@harrache.info>
2012-12-28 17:54:11 +0100

    globals pi et deg

--- utils.tcl
+++ utils.tcl
@@ -12,6 +12,10 @@ proc abspath {arg args} {
     eval $cmd
 }
 
+#### globals
+set ::pi  [expr 4 * atan(1)]
+set ::deg [expr $pi / 180]
+
 # # Sound
 load [abspath lib snack libsnack.so]
 load [abspath lib snack libsound.so]
@@ -24,6 +28,66 @@ proc lremove {liste quoi} {
 
 proc random {{range 10}} {
     return [expr {int(rand()*$range)}]
+}
+
+proc hsvToRgb {h s v} {
+    set Hi [expr { int( double($h) / 60 ) % 6 }]
+    set f [expr { double($h) / 60 - $Hi }]
+    set s [expr { double($s)/255 }]
+    set v [expr { double($v)/255 }]
+    set p [expr { double($v) * (1 - $s) }]
+    set q [expr { double($v) * (1 - $f * $s) }]
+    set t [expr { double($v) * (1 - (1 - $f) * $s) }]
+    switch -- $Hi {
+        0 {
+            set r $v
+            set g $t
+            set b $p
+        }
+        1 {
+            set r $q
+            set g $v
+            set b $p
+        }
+        2 {
+            set r $p
+            set g $v
+            set b $t
+        }
+        3 {
+            set r $p
+            set g $q
+            set b $v
+        }
+        4 {
+            set r $t
+            set g $p
+            set b $v
+        }
+        5 {
+            set r $v
+            set g $p
+            set b $q
+        }
+        default {
+            error "Wrong Hi value in hsvToRgb procedure! This should never happen!"
+        }
+    }
+    set r [expr {round($r*255)}]
+    set g [expr {round($g*255)}]
+    set b [expr {round($b*255)}]
+    return [list $r $g $b]
+ }
+
+
+proc get_random_color {} {
+    set h [expr { int(256 * rand()) }]
+    set s [expr { int(256 * rand()) }]
+    set v [expr { int(256 * rand()) }]
+
+    lassign [hsvToRgb $h $s $v] r g b
+
+    return [format "#%02x%02x%02x" $r $g $b]
 }
 
 proc is_main {} {
