4b286e929c8d6723c89f6ecd31458bd7e106eda3
Salem Harrache <salem@harrache.info>
2012-12-28 14:09:36 +0100

    Ajout de set_binding, decr_ang, incr_ang, speed_up, speed_down

--- player.tcl
+++ player.tcl
@@ -4,25 +4,57 @@ generate_pac_accessors Player score
 generate_pac_accessors Player id
 generate_pac_accessors Player name
 
+
 method PlayerAbstraction init {} {
     this set_name [lindex [split "$objName" "_"] [expr {[llength [split "$objName" "_"]] - 2}]]
     this set_id [get_new_id]
     this set_score 10
 }
 
+
 method PlayerPresentation init {} {
     set this(label) [label $this(tk_parent).label -justify right -text [this get_label_message]]
     pack $this(label) -expand 1 -fill both
 }
 
+
 method PlayerPresentation get_label_message {} {
     return "Joueur [this get_id] ([this get_name]): [this get_score]"
 }
+
 
 method PlayerPresentation refresh {} {
     set this(label) configure -text [this get_label_message]
 }
 
+
 method PlayerControl init {} {
+
+}
+
+
+method PlayerControl set_binding {config} {
+    eval $config
+}
+
+
+method PlayerControl decr_ang {} {
+
+}
+
+
+method PlayerControl incr_ang {} {
+
+}
+
+method PlayerControl speed_up {} {
+
+}
+
+method PlayerControl speed_down {} {
+
+}
+
+method PlayerControl shut {} {
 
 }
