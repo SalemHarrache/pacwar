eb62227401c0830ecb2171dcb837bd9d49bfb906
Salem Harrache <salem@harrache.info>
2012-12-29 12:03:04 +0100

    universe add_ship_callback avec le noyau

--- universe.tcl
+++ universe.tcl
@@ -1,6 +1,8 @@
 # Génération d'un agent pac multivue : MapUniverse et MiniMapUniverse
 generate_pac_agent_multi_view "Universe" [list "Map" "MiniMap"]
 
+generate_pac_parent_accessors Universe kernel
+generate_pac_accessors Universe kernel
 # Génération d'un methode pour acceder au canvas_map parent depuis universe.
 generate_pac_parent_accessors Universe canvas_map
 generate_pac_parent_accessors Universe canvas_mini_map
@@ -14,17 +16,34 @@ generate_pac_presentation_accessors MapUniverse num_background
 generate_pac_presentation_accessors MiniMapUniverse canvas_mini_map
 
 
+# UniverseAbstraction ##
+method UniverseAbstraction init {} {
+    this set_kernel [$this(control) get_parent_kernel]
+    $this(kernel) Subscribe_after_Add_new_planet A "$this(control) add_planet_callback \$rep \$x \$y \$radius \$density"
+    # $this(kernel) Subscribe_after_Update_planet A "$this(control) update_planet \$id \$D_update"
+    $this(kernel) Subscribe_after_Add_new_ship A "$this(control) add_ship_callback \$rep \$x \$y \$radius"
+    # $this(kernel) Subscribe_after_Update_ship A "$this(control) update_ship \$id \$D_update"
+    # $this(kernel) Subscribe_after_Start_fire A "$this(control) start_fire \$rep \$this(L_bullets)"
+    # $this(kernel) Subscribe_after_Compute_a_simulation_step A "$this(control) update_fire \$rep \$this(L_bullets)"
+    # $this(kernel) Subscribe_after_Destroy_ship A "$this(control) destroy_ship \$rep \$id"
+}
+
+# UniverseControl ##
 method UniverseControl init {} {
     $this(parent) set_universe $objName
     set this(ships) [list]
 }
 
-
-method UniverseControl add_ship {name position_x position_y} {
-    set new_ship [ShipControl ship_$name $objName ""]
-    $new_ship set_position $position_x $position_y
+method UniverseControl add_ship_callback {id x y r} {
+    set new_ship [ShipControl $id $objName ""]
+    $new_ship set_position $x $y
     lappend this(ships) $new_ship
-    return [$new_ship get_id]
+}
+
+method UniverseControl add_planet_callback {id x y radius density} {
+    set new_ship [ShipControl $id $objName ""]
+    $new_ship set_position $x $y
+    lappend this(ships) $new_ship
 }
 
 
@@ -40,6 +59,8 @@ method UniverseControl send_event_to_ship {event ship_id} {
     [this get_ship $ship_id] $event
 }
 
+
+# MapUniversePresentation ##
 method MapUniversePresentation init {} {
     this set_num_background 0
     this set_canvas_map [$this(control) get_parent_canvas_map]
@@ -77,6 +98,8 @@ method MapUniversePresentation switch_background {} {
     initBackground $this(canvas_map) [get_new_universe_bg $this(num_background)]
 }
 
+
+# MiniMapUniversePresentation ##
 method MiniMapUniversePresentation init {} {
     this set_canvas_mini_map [$this(control) get_parent_canvas_mini_map]
     $this(canvas_mini_map) configure -width 200 -height 200 -background "#1E1E1E"
