8dec474cd470b748a2e9e4600426bb9276a8b96b
Salem Harrache <salem@harrache.info>
2012-12-29 13:30:51 +0100

    ajout de l'agent pac planet

--- planet.tcl
+++ planet.tcl
@@ -1,107 +1,65 @@
 generate_pac_agent_multi_view Planet [list "Map" "MiniMap"]
 generate_pac_accessors Planet id
-generate_pac_accessors Planet name
+generate_pac_accessors Planet position_x
+generate_pac_accessors Planet position_y
+generate_pac_accessors Planet radius
+generate_pac_accessors Planet density
+
 generate_pac_parent_accessors Planet canvas_map
 generate_pac_parent_accessors Planet canvas_mini_map
 
-
 generate_pac_parent_accessors MapPlanet canvas_map
 generate_pac_parent_accessors MiniMapPlanet canvas_mini_map
+
 generate_pac_presentation_accessors MapPlanet canvas_map
+generate_pac_presentation_accessors MapPlanet bg_image
 generate_pac_presentation_accessors MiniMapPlanet canvas_mini_map
 
-generate_pac_parent_accessors MapPlanet name
-generate_pac_parent_accessors MiniMapPlanet name
-generate_pac_presentation_accessors MapPlanet name
-generate_pac_presentation_accessors MiniMapPlanet name
-
-generate_pac_parent_accessors MapPlanet id
-generate_pac_parent_accessors MiniMapPlanet id
 generate_pac_presentation_accessors MapPlanet id
 generate_pac_presentation_accessors MiniMapPlanet id
-
-generate_pac_presentation_accessors MapPlanet radius
-generate_pac_presentation_accessors MiniMapPlanet radius
 
 
 
 method PlanetAbstraction init {} {
-    this set_name [lindex [split "$objName" "_"] 1]
     this set_id $objName
 }
 
-method PlanetControl init {} {
-}
-
-method PlanetControl set_position {x y} {
-    $this(map) set_position $x $y
-    $this(minimap) set_position $x $y
-}
-
-method PlanetControl move_left {} {
-    $this(map)  move -1 0
-    $this(minimap)  move -1 0
-}
-
-method PlanetControl move_right {} {
-    $this(map)  move 1 0
-    $this(minimap)  move 1 0
-}
-
-method PlanetControl move_up {} {
-    $this(map)  move 0 -1
-    $this(minimap)  move 0 -1
-}
-
-method PlanetControl move_down {} {
-    $this(map)  move 0 1
-    $this(minimap)  move 0 1
-}
-
-method PlanetControl shut {} {
-    puts "shut"
+method PlanetControl draw {} {
+	[$this(map) attribute presentation] draw [this get_position_x] [this get_position_y] [this get_radius]
+	[$this(minimap) attribute presentation] draw [expr ([this get_position_x] / 10)] \
+												 [expr ([this get_position_y] / 10)] \
+												 [expr ([this get_radius] / 10)]
 }
 
 
-method MapPlanetControl move {x y} {
-    $this(presentation) move $x $y
-}
-
-method MapPlanetControl set_position {x y} {
-    $this(presentation) move $x $y
-}
 
 method MapPlanetPresentation init {} {
-    this set_name [$this(control) get_parent_name]
-    this set_id "map_ship_[$this(control) get_parent_id]"
+    this set_bg_image [get_random_planet_bg]
     this set_canvas_map [$this(control) get_parent_canvas_map]
-    set bg [get_ship_bg $this(name)]
-    this set_radius [expr int([image height $bg] / 2)]
-    this set_id [$this(canvas_map) create image 0  0 -anchor nw -image [get_ship_bg $this(name)]]
 }
 
-method MapPlanetPresentation move {x y} {
-    move_canvas $this(canvas_map) [this get_id] [expr $x * 10] [expr $y * 10]
+method MapPlanetPresentation draw {x y radius} {
+	this set_id [$this(canvas_map) create image $x  $y -anchor center -image [this get_bg_image]]
+}
+
+method MapPlanetPresentation delete {} {
+	$this(canvas_map) delete [this get_id]
 }
 
 
-method MiniMapPlanetControl move {x y} {
-    $this(presentation) move $x $y
-}
-
-method MiniMapPlanetControl set_position {x y} {
-    $this(presentation) move $x $y
-}
 
 method MiniMapPlanetPresentation init {} {
-    this set_name [$this(control) get_parent_name]
-    this set_id "mini_map_ship_[$this(control) get_parent_id]"
     this set_canvas_mini_map [$this(control) get_parent_canvas_mini_map]
-    set bg [get_ship_bg $this(name)]
-    this set_radius 5
-    this set_id [$this(canvas_mini_map) create oval 0 0 10 10 -outline [get_random_color] -fill [get_random_color]]
 }
 
-method MiniMapPlanetPresentation move {x y} {
-    move_canvas $this(canvas_mini_map) [this get_id] $x $y
+method MiniMapPlanetPresentation draw {x y radius} {
+    set x1 [expr $x - $radius]
+    set y1 [expr $y - $radius]
+    set x2 [expr $x + $radius]
+    set y2 [expr $y + $radius]
+    this set_id [$this(canvas_mini_map) create oval $x1 $y1 $x2 $y2 -outline #FFF -fill [get_random_color]]
+}
+
+method MiniMapPlanetPresentation delete {} {
+	$this(canvas_mini_map) delete [this get_id]
 }
