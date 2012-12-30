90de1eab426e5631a2137532a14ca2c665ff18e3
Salem Harrache <salem@harrache.info>
2012-12-30 10:51:42 +0100

    Supprimer le score et ajouter un status

--- player.tcl
+++ player.tcl
@@ -1,31 +1,32 @@
-
 generate_pac_agent Player
-generate_pac_accessors Player score
 generate_pac_accessors Player id
 generate_pac_accessors Player name
+generate_pac_accessors Player status
 
 
 method PlayerAbstraction init {} {
     set infos [split "$objName" "_"]
     this set_name [lindex $infos [expr ([llength $infos] - 2)]]
-    this set_id "[lindex $infos [expr ([llength $infos] - 3)]]"
-    this set_score 10
+    this set_id [lindex $infos [expr ([llength $infos] - 3)]]
+    set this(status) 1
 }
-
 
 method PlayerPresentation init {} {
-    set this(label) [label $this(tk_parent).label -justify right -text [this get_label_message]]
+    set this(label) [label $this(tk_parent).label -justify right]
     pack $this(label) -expand 1 -fill both
+    this refresh
 }
-
-
-method PlayerPresentation get_label_message {} {
-    return "Joueur [this get_id] ([this get_name]): [this get_score]"
-}
-
 
 method PlayerPresentation refresh {} {
-    set this(label) configure -text [this get_label_message]
+    if {[this get_status] == 1} {
+        $this(label) configure -text "Joueur [this get_id] ([this get_name]) : Alive"
+    } else {
+        $this(label) configure -text "Joueur [this get_id] ([this get_name]) : Dead" -foreground red
+    }
+}
+
+method PlayerPresentation set_status {v} {
+    this refresh
 }
 
 method PlayerControl set_binding {} {
@@ -33,26 +34,7 @@ method PlayerControl set_binding {} {
 }
 
 method PlayerControl send_event {event} {
-    $this(parent) send_event_from_player $event [this get_id]
-}
-
-
-method PlayerControl move_left {} {
-    this send_event "move_left"
-}
-
-method PlayerControl move_right {} {
-    this send_event "move_right"
-}
-
-method PlayerControl move_up {} {
-    this send_event "move_up"
-}
-
-method PlayerControl move_down {} {
-    this send_event "move_down"
-}
-
-method PlayerControl shut {} {
-    this send_event "shut"
+    if {[this get_status] == 1} {
+        $this(parent) send_event_from_player $event [this get_id]
+    }
 }
