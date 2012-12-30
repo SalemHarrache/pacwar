837ed068fb06f3933dacbc9517ee8ceb4e2c2ab2
Salem Harrache <salem@harrache.info>
2012-12-30 10:54:27 +0100

    Pac Sound a que deux son, battle et gameover

--- sound.tcl
+++ sound.tcl
@@ -1,11 +1,9 @@
 
 # generate pac agent without abstraction
-generate_pac_agent "Sound" 1 0 1
-generate_pac_accessors "Sound" main_sound
+generate_pac_agent "Sound" 1 0
 generate_pac_accessors "Sound" battle_sound
 generate_pac_accessors "Sound" end_sound
 
-# ==> left volume
 generate_pac_accessors "Sound" mode
 generate_pac_accessors "Sound" volume
 
@@ -13,7 +11,6 @@ generate_pac_accessors "Sound" volume
 
 
 method SoundAbstraction init {} {
-    this set_main_sound [snack::sound main_sound -file [abspath ressources sound main.mp3]]
     this set_battle_sound [snack::sound battle_sound -file [abspath ressources sound battle.mp3]]
     this set_end_sound [snack::sound end_sound -file [abspath ressources sound end.mp3]]
     this set_mode "battle"
@@ -21,7 +18,6 @@ method SoundAbstraction init {} {
 }
 
 method SoundAbstraction stop_all {} {
-    $this(main_sound) stop
     $this(battle_sound) stop
     $this(end_sound) stop
 }
@@ -29,16 +25,10 @@ method SoundAbstraction stop_all {} {
 method SoundAbstraction switch_mode {mod} {
     this stop_all
     this set_mode $mod
-    if {$mod=="main"} {
-        loop_sound $this(main_sound)
-    } else {
-        if {$mod=="battle"} {
-            loop_sound $this(battle_sound)
-        } else {
-            if {$mod=="end"} {
-                $this(end_sound) play
-            }
-        }
+    if {$mod=="battle"} {
+        loop_sound $this(battle_sound)
+    } elseif {$mod=="end"} {
+        $this(end_sound) play
     }
 }
 
@@ -65,6 +55,6 @@ method SoundControl toggle_sound {} {
     }
 }
 
-method SoundControl system_change_volume { v } {
+method SoundControl system_change_volume {v} {
     $this(parent) sound_changed $v
 }
